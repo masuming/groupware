30333aad2005e509e7c37c290c83e2fe40cd706b
masuming <mail@masuming.ivory.ne.jp>
2021-04-11 12:01:24 +0900

    index_nav.php「トップページナビ」ヘッダーサブタイトル修正

--- index_nav.php
+++ index_nav.php
@@ -1,7 +1,7 @@
 <div class="wrapper">
 <header>
     <div class="header__inner">
-    <p class="header__subtitle"><small>サークル・メンバーの情報共有ツール「MM GROUPWARE」</small></p>
+    <p class="header__subtitle"><small>サークル・メンバーの情報共有ツール | MM GROUPWARE |</small></p>  
     <h1><a href="index.php"></a></h1>
 
 </header>
